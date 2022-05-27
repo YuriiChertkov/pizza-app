4a52f11bf84482a79b622e0641a80162c9473b17
YuriiChertkov <yuriichertkov@gmail.com>
2022-05-27 13:05:41 +0300

    create cart page template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,17 +18,17 @@ export const Home = () => {
       });
   }, []);
   return (
-    <>
-      <div className='content__top'>
+    <div className="container">
+      <div className="content__top">
         <Categories />
         <Sort />
       </div>
-      <h2 className='content__title'>Все пиццы</h2>
-      <div className='content__items'>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
         {isLoading
           ? [...new Array(8)].map((_, i) => <PizzaLoader key={i} />)
           : pizzasItems.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
