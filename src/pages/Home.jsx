2d72dfb35503f2018cb1993f2b38a4aba0d2dd14
YuriiChertkov <yuriichertkov@gmail.com>
2022-06-06 15:50:24 +0300

    redux cat prt2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,33 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { AppContext } from "../App";
 import { Categories } from "../components/Categories";
 import Pagination from "../components/Pagination";
 import { PizzaBlock } from "../components/PizzaBlock";
 import { PizzaLoader } from "../components/PizzaBlock/Loader";
 import { Sort } from "../components/Sort";
+import { setCategoryId } from "../redux/slices/filterSlice";
 
 export const Home = () => {
+  const categoryId = useSelector((state) => state.filter.categoryId);
+  const dispatch = useDispatch();
+
   const { searchValue } = React.useContext(AppContext);
   const [pizzasItems, setPizzasItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [category, setCategory] = React.useState(0);
+  //const [category, setCategory] = React.useState(0);
   const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "популярности",
     sortValue: "rating",
   });
 
+  onClickCategory = (id) => dispatch(setCategoryId(id));
+
   React.useEffect(() => {
     const sortProperty = sortType.sortValue.replace("-", "");
     const orderProperty = sortType.sortValue.includes("-") ? "desc" : "asc";
-    const categoryAllProperty = category > 0 ? `category=${category}` : ``;
+    const categoryAllProperty = categoryId > 0 ? `category=${categoryId}` : ``;
     const search = searchValue ? `&search=${searchValue}` : ``;
 
     setIsLoading(true);
@@ -37,7 +44,7 @@ export const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [category, sortType, searchValue, currentPage]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = pizzasItems
     .filter((obj) => {
@@ -51,10 +58,7 @@ export const Home = () => {
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          value={category}
-          onClickCategory={(id) => setCategory(id)}
-        />
+        <Categories value={categoryId} onClickCategory={onClickCategory} />
         <Sort value={sortType} onClickSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,30 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: 0,
+  categoryId: 0,
+  sort: {
+    name: "популярности",
+    sortValue: "rating",
+  },
 };
 
 export const filterSlice = createSlice({
   name: "filter",
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    setCategoryId: (state, action) => {
+      console.log(action);
+      state.categoryId = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = filterSlice.actions;
+export const { setCategoryId } = filterSlice.actions;
 
 export default filterSlice.reducer;
