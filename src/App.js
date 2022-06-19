c9cff6bfc92f1c81bec499bc9df2a91cefccfa06
YuriiChertkov <yuriichertkov@gmail.com>
2022-06-19 14:29:16 +0300

    using Outlet hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
-import { Header } from "./components/Header";
+
 import { Home } from "./pages/Home";
 import { Cart } from "./pages/Cart";
 import "./scss/app.scss";
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from "./pages/404";
 import { FullPizza } from "./pages/FullPizza";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <div className='wrapper'>
-      <Header />
-      <div className='content'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/pizza/:id' element={<FullPizza />} />
-          <Route path='/cart' element={<Cart />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path='/pizza/:id' element={<FullPizza />} />
+        <Route path='/cart' element={<Cart />} />
+      </Route>
+    </Routes>
   );
 }
 
